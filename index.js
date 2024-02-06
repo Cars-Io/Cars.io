4e5cda0fc578cc8ca5bddf1f45ae078b3bfe05bb
Cars-Io <159088499+Cars-Io@users.noreply.github.com>
2024-02-05 21:18:15 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,758 @@
+let count = 0;
+
+document.getElementById("increaseBtn").onclick = function(){
+    count+=1400;
+    document.getElementById("countLabel").innerHTML = count;
+}
+
+
+function decreaseBtn1(){
+    count-=25000;
+    document.getElementById("countLabel").innerHTML = count;
+}
+
+function quantBtn1(){
+    var element = document.getElementById('quantLabel1')
+    var value = element.innerHTML;
+
+    ++value;
+
+    console.log(value);
+    document.getElementById('quantLabel1').innerHTML = value;
+}
+
+
+function decreaseBtn2(){
+    count-=30000;
+    document.getElementById("countLabel").innerHTML = count;
+}
+
+function quantBtn2(){
+    var element = document.getElementById('quantLabel2')
+    var value = element.innerHTML;
+
+    ++value;
+
+    console.log(value);
+    document.getElementById('quantLabel2').innerHTML = value;
+}
+
+
+function decreaseBtn3(){
+    count-=45000;
+    document.getElementById("countLabel").innerHTML = count;
+}
+
+function quantBtn3(){
+    var element = document.getElementById('quantLabel3')
+    var value = element.innerHTML;
+
+    ++value;
+
+    console.log(value);
+    document.getElementById('quantLabel3').innerHTML = value;
+}
+
+
+function decreaseBtn4(){
+    count-=60200;
+    document.getElementById("countLabel").innerHTML = count;
+}
+
+function quantBtn4(){
+    var element = document.getElementById('quantLabel4')
+    var value = element.innerHTML;
+
+    ++value;
+
+    console.log(value);
+    document.getElementById('quantLabel4').innerHTML = value;
+}
+
+
+function decreaseBtn5(){
+    count-=81500;
+    document.getElementById("countLabel").innerHTML = count;
+}
+
+function quantBtn5(){
+    var element = document.getElementById('quantLabel5')
+    var value = element.innerHTML;
+
+    ++value;
+
+    console.log(value);
+    document.getElementById('quantLabel5').innerHTML = value;
+}
+
+
+function decreaseBtn6(){
+    count-=100200;
+    document.getElementById("countLabel").innerHTML = count;
+}
+
+function quantBtn6(){
+    var element = document.getElementById('quantLabel6')
+    var value = element.innerHTML;
+
+    ++value;
+
+    console.log(value);
+    document.getElementById('quantLabel6').innerHTML = value;
+}
+
+
+function decreaseBtn7(){
+    count-=148000;
+    document.getElementById("countLabel").innerHTML = count;
+}
+
+function quantBtn7(){
+    var element = document.getElementById('quantLabel7')
+    var value = element.innerHTML;
+
+    ++value;
+
+    console.log(value);
+    document.getElementById('quantLabel7').innerHTML = value;
+}
+
+
+function decreaseBtn8(){
+    count-=195600;
+    document.getElementById("countLabel").innerHTML = count;
+}
+
+function quantBtn8(){
+    var element = document.getElementById('quantLabel8')
+    var value = element.innerHTML;
+
+    ++value;
+
+    console.log(value);
+    document.getElementById('quantLabel8').innerHTML = value;
+}
+
+
+function decreaseBtn9(){
+    count-=200000;
+    document.getElementById("countLabel").innerHTML = count;
+}
+
+function quantBtn9(){
+    var element = document.getElementById('quantLabel9')
+    var value = element.innerHTML;
+
+    ++value;
+
+    console.log(value);
+    document.getElementById('quantLabel9').innerHTML = value;
+}
+
+
+function decreaseBtn10(){
+    count-=250800;
+    document.getElementById("countLabel").innerHTML = count;
+}
+
+function quantBtn10(){
+    var element = document.getElementById('quantLabel10')
+    var value = element.innerHTML;
+
+    ++value;
+
+    console.log(value);
+    document.getElementById('quantLabel10').innerHTML = value;
+}
+
+
+function decreaseBtn11(){
+    count-=295050;
+    document.getElementById("countLabel").innerHTML = count;
+}
+
+function quantBtn11(){
+    var element = document.getElementById('quantLabel11')
+    var value = element.innerHTML;
+
+    ++value;
+
+    console.log(value);
+    document.getElementById('quantLabel11').innerHTML = value;
+}
+
+
+function decreaseBtn12(){
+    count-=305800;
+    document.getElementById("countLabel").innerHTML = count;
+}
+
+function quantBtn12(){
+    var element = document.getElementById('quantLabel12')
+    var value = element.innerHTML;
+
+    ++value;
+
+    console.log(value);
+    document.getElementById('quantLabel12').innerHTML = value;
+}
+
+
+function decreaseBtn13(){
+    count-=399090;
+    document.getElementById("countLabel").innerHTML = count;
+}
+
+function quantBtn13(){
+    var element = document.getElementById('quantLabel13')
+    var value = element.innerHTML;
+
+    ++value;
+
+    console.log(value);
+    document.getElementById('quantLabel13').innerHTML = value;
+}
+
+
+function decreaseBtn14(){
+    count-=500000;
+    document.getElementById("countLabel").innerHTML = count;
+}
+
+function quantBtn14(){
+    var element = document.getElementById('quantLabel14')
+    var value = element.innerHTML;
+
+    ++value;
+
+    console.log(value);
+    document.getElementById('quantLabel14').innerHTML = value;
+}
+
+
+function decreaseBtn15(){
+    count-=545500;
+    document.getElementById("countLabel").innerHTML = count;
+}
+
+function quantBtn15(){
+    var element = document.getElementById('quantLabel15')
+    var value = element.innerHTML;
+
+    ++value;
+
+    console.log(value);
+    document.getElementById('quantLabel15').innerHTML = value;
+}
+
+
+function decreaseBtn16(){
+    count-=580900;
+    document.getElementById("countLabel").innerHTML = count;
+}
+
+function quantBtn16(){
+    var element = document.getElementById('quantLabel16')
+    var value = element.innerHTML;
+
+    ++value;
+
+    console.log(value);
+    document.getElementById('quantLabel16').innerHTML = value;
+}
+
+
+function decreaseBtn17(){
+    count-=621400;
+    document.getElementById("countLabel").innerHTML = count;
+}
+
+function quantBtn17(){
+    var element = document.getElementById('quantLabel17')
+    var value = element.innerHTML;
+
+    ++value;
+
+    console.log(value);
+    document.getElementById('quantLabel17').innerHTML = value;
+}
+
+
+function decreaseBtn18(){
+    count-=688900;
+    document.getElementById("countLabel").innerHTML = count;
+}
+
+function quantBtn18(){
+    var element = document.getElementById('quantLabel18')
+    var value = element.innerHTML;
+
+    ++value;
+
+    console.log(value);
+    document.getElementById('quantLabel18').innerHTML = value;
+}
+
+
+function decreaseBtn19(){
+    count-=791100;
+    document.getElementById("countLabel").innerHTML = count;
+}
+
+function quantBtn19(){
+    var element = document.getElementById('quantLabel19')
+    var value = element.innerHTML;
+
+    ++value;
+
+    console.log(value);
+    document.getElementById('quantLabel19').innerHTML = value;
+}
+
+
+function decreaseBtn20(){
+    count-=825900;
+    document.getElementById("countLabel").innerHTML = count;
+}
+
+function quantBtn20(){
+    var element = document.getElementById('quantLabel20')
+    var value = element.innerHTML;
+
+    ++value;
+
+    console.log(value);
+    document.getElementById('quantLabel20').innerHTML = value;
+}
+
+
+function decreaseBtn21(){
+    count-=850000;
+    document.getElementById("countLabel").innerHTML = count;
+}
+
+function quantBtn21(){
+    var element = document.getElementById('quantLabel21')
+    var value = element.innerHTML;
+
+    ++value;
+
+    console.log(value);
+    document.getElementById('quantLabel21').innerHTML = value;
+}
+
+
+function decreaseBtn22(){
+    count-=970120;
+    document.getElementById("countLabel").innerHTML = count;
+}
+
+function quantBtn22(){
+    var element = document.getElementById('quantLabel22')
+    var value = element.innerHTML;
+
+    ++value;
+
+    console.log(value);
+    document.getElementById('quantLabel22').innerHTML = value;
+}
+
+
+function decreaseBtn23(){
+    count-=1040000;
+    document.getElementById("countLabel").innerHTML = count;
+}
+
+function quantBtn23(){
+    var element = document.getElementById('quantLabel23')
+    var value = element.innerHTML;
+
+    ++value;
+
+    console.log(value);
+    document.getElementById('quantLabel23').innerHTML = value;
+}
+
+
+function decreaseBtn24(){
+    count-=1200000;
+    document.getElementById("countLabel").innerHTML = count;
+}
+
+function quantBtn24(){
+    var element = document.getElementById('quantLabel24')
+    var value = element.innerHTML;
+
+    ++value;
+
+    console.log(value);
+    document.getElementById('quantLabel24').innerHTML = value;
+}
+
+
+function decreaseBtn25(){
+    count-=1345000;
+    document.getElementById("countLabel").innerHTML = count;
+}
+
+function quantBtn25(){
+    var element = document.getElementById('quantLabel25')
+    var value = element.innerHTML;
+
+    ++value;
+
+    console.log(value);
+    document.getElementById('quantLabel25').innerHTML = value;
+}
+
+
+function decreaseBtn26(){
+    count-=1500600;
+    document.getElementById("countLabel").innerHTML = count;
+}
+
+function quantBtn26(){
+    var element = document.getElementById('quantLabel26')
+    var value = element.innerHTML;
+
+    ++value;
+
+    console.log(value);
+    document.getElementById('quantLabel26').innerHTML = value;
+}
+
+
+function decreaseBtn27(){
+    count-=2100560;
+    document.getElementById("countLabel").innerHTML = count;
+}
+
+function quantBtn27(){
+    var element = document.getElementById('quantLabel27')
+    var value = element.innerHTML;
+
+    ++value;
+
+    console.log(value);
+    document.getElementById('quantLabel27').innerHTML = value;
+}
+
+
+function decreaseBtn28(){
+    count-=2710900;
+    document.getElementById("countLabel").innerHTML = count;
+}
+
+function quantBtn28(){
+    var element = document.getElementById('quantLabel28')
+    var value = element.innerHTML;
+
+    ++value;
+
+    console.log(value);
+    document.getElementById('quantLabel28').innerHTML = value;
+}
+
+
+function decreaseBtn29(){
+    count-=3250000;
+    document.getElementById("countLabel").innerHTML = count;
+}
+
+function quantBtn29(){
+    var element = document.getElementById('quantLabel29')
+    var value = element.innerHTML;
+
+    ++value;
+
+    console.log(value);
+    document.getElementById('quantLabel29').innerHTML = value;
+}
+
+
+function decreaseBtn30(){
+    count-=3680090;
+    document.getElementById("countLabel").innerHTML = count;
+}
+
+function quantBtn30(){
+    var element = document.getElementById('quantLabel30')
+    var value = element.innerHTML;
+
+    ++value;
+
+    console.log(value);
+    document.getElementById('quantLabel30').innerHTML = value;
+}
+
+
+function decreaseBtn31(){
+    count-=3899050;
+    document.getElementById("countLabel").innerHTML = count;
+}
+
+function quantBtn31(){
+    var element = document.getElementById('quantLabel31')
+    var value = element.innerHTML;
+
+    ++value;
+
+    console.log(value);
+    document.getElementById('quantLabel31').innerHTML = value;
+}
+
+
+function decreaseBtn32(){
+    count-=4998090;
+    document.getElementById("countLabel").innerHTML = count;
+}
+
+function quantBtn32(){
+    var element = document.getElementById('quantLabel32')
+    var value = element.innerHTML;
+
+    ++value;
+
+    console.log(value);
+    document.getElementById('quantLabel32').innerHTML = value;
+}
+
+
+function decreaseBtn33(){
+    count-=6999000;
+    document.getElementById("countLabel").innerHTML = count;
+}
+
+function quantBtn33(){
+    var element = document.getElementById('quantLabel33')
+    var value = element.innerHTML;
+
+    ++value;
+
+    console.log(value);
+    document.getElementById('quantLabel33').innerHTML = value;
+}
+
+
+function decreaseBtn34(){
+    count-=7290000;
+    document.getElementById("countLabel").innerHTML = count;
+}
+
+function quantBtn34(){
+    var element = document.getElementById('quantLabel34')
+    var value = element.innerHTML;
+
+    ++value;
+
+    console.log(value);
+    document.getElementById('quantLabel34').innerHTML = value;
+}
+
+
+function decreaseBtn35(){
+    count-=7999990;
+    document.getElementById("countLabel").innerHTML = count;
+}
+
+function quantBtn35(){
+    var element = document.getElementById('quantLabel35')
+    var value = element.innerHTML;
+
+    ++value;
+
+    console.log(value);
+    document.getElementById('quantLabel35').innerHTML = value;
+}
+
+
+function decreaseBtn36(){
+    count-=8990800;
+    document.getElementById("countLabel").innerHTML = count;
+}
+
+function quantBtn36(){
+    var element = document.getElementById('quantLabel36')
+    var value = element.innerHTML;
+
+    ++value;
+
+    console.log(value);
+    document.getElementById('quantLabel36').innerHTML = value;
+}
+
+
+function decreaseBtn37(){
+    count-=35000760;
+    document.getElementById("countLabel").innerHTML = count;
+}
+
+function quantBtn37(){
+    var element = document.getElementById('quantLabel37')
+    var value = element.innerHTML;
+
+    ++value;
+
+    console.log(value);
+    document.getElementById('quantLabel37').innerHTML = value;
+}
+
+
+function decreaseBtn38(){
+    count-=40120800;
+    document.getElementById("countLabel").innerHTML = count;
+}
+
+function quantBtn38(){
+    var element = document.getElementById('quantLabel38')
+    var value = element.innerHTML;
+
+    ++value;
+
+    console.log(value);
+    document.getElementById('quantLabel38').innerHTML = value;
+}
+
+
+function decreaseBtn39(){
+    count-=45135900;
+    document.getElementById("countLabel").innerHTML = count;
+}
+
+function quantBtn39(){
+    var element = document.getElementById('quantLabel39')
+    var value = element.innerHTML;
+
+    ++value;
+
+    console.log(value);
+    document.getElementById('quantLabel39').innerHTML = value;
+}
+
+
+function decreaseBtn40(){
+    count-=55065760;
+    document.getElementById("countLabel").innerHTML = count;
+}
+
+function quantBtn40(){
+    var element = document.getElementById('quantLabel40')
+    var value = element.innerHTML;
+
+    ++value;
+
+    console.log(value);
+    document.getElementById('quantLabel40').innerHTML = value;
+}
+
+
+function decreaseBtn41(){
+    count-=65190300;
+    document.getElementById("countLabel").innerHTML = count;
+}
+
+function quantBtn41(){
+    var element = document.getElementById('quantLabel41')
+    var value = element.innerHTML;
+
+    ++value;
+
+    console.log(value);
+    document.getElementById('quantLabel41').innerHTML = value;
+}
+
+
+function decreaseBtn42(){
+    count-=72215440;
+    document.getElementById("countLabel").innerHTML = count;
+}
+
+function quantBtn42(){
+    var element = document.getElementById('quantLabel42')
+    var value = element.innerHTML;
+
+    ++value;
+
+    console.log(value);
+    document.getElementById('quantLabel42').innerHTML = value;
+}
+
+
+function decreaseBtn43(){
+    count-=87548320;
+    document.getElementById("countLabel").innerHTML = count;
+}
+
+function quantBtn43(){
+    var element = document.getElementById('quantLabel43')
+    var value = element.innerHTML;
+
+    ++value;
+
+    console.log(value);
+    document.getElementById('quantLabel43').innerHTML = value;
+}
+
+
+function decreaseBtn44(){
+    count-=93232590;
+    document.getElementById("countLabel").innerHTML = count;
+}
+
+function quantBtn44(){
+    var element = document.getElementById('quantLabel44')
+    var value = element.innerHTML;
+
+    ++value;
+
+    console.log(value);
+    document.getElementById('quantLabel44').innerHTML = value;
+}
+
+
+function decreaseBtn45(){
+    count-=139599600;
+    document.getElementById("countLabel").innerHTML = count;
+}
+
+function quantBtn45(){
+    var element = document.getElementById('quantLabel45')
+    var value = element.innerHTML;
+
+    ++value;
+
+    console.log(value);
+    document.getElementById('quantLabel45').innerHTML = value;
+}
+
+
+function decreaseBtn46(){
+    count-=149571000;
+    document.getElementById("countLabel").innerHTML = count;
+}
+
+function quantBtn46(){
+    var element = document.getElementById('quantLabel46')
+    var value = element.innerHTML;
+
+    ++value;
+
+    console.log(value);
+    document.getElementById('quantLabel46').innerHTML = value;
+}
+
+
+function decreaseBtn47(){
+    count-=8000000000000000;
+    document.getElementById("countLabel").innerHTML = count;
+}
+
+function quantBtn47(){
+    var element = document.getElementById('quantLabel47')
+    var value = element.innerHTML;
+
+    ++value;
+
+    console.log(value);
+    document.getElementById('quantLabel47').innerHTML = value;
+}
